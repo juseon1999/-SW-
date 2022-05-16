57c266f207f627760f7ab9a97a41437955b0feab
juseon1999 <105285562+juseon1999@users.noreply.github.com>
2022-05-16 23:42:36 +0900

    Add files via upload
    Hello World!

---
+++ c-programing/0510/ex02.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(void)
+{
+	int a=10;
+	if (a > 0);
+	printf("good");
+	return 0;
+}
---
+++ c-programing/0510/ex03.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main(void)
+{
+	char ch1, ch2, ch3, ch4;
+	ch1 = 'Y', ch2 = 'e', ch3 = 's', ch4 = ',';
+	printf("%c%c%c%c%s", ch1, ch2, ch3, ch4, "i am a computer.");
+	printf(" %s","feel so good");
+	return 0;
+}
